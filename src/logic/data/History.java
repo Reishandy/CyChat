6dbbff77015db7bcb3057f5ac0f395a8f9668970
Reishandy <isthisruxury@gmail.com>
2023-10-13 15:58:06 +0800

    Major reform

--- src/logic/data/History.java
+++
@@ -1,3 +0,0 @@
-package logic.data;
-
-public record History(String userName, String dateTime, String message) {}
--- src/logic/data/ManagersWrapper.java
+++
@@ -1,7 +0,0 @@
-package logic.data;
-
-import logic.manager.ContactManager;
-import logic.manager.PeerManager;
-
-public record ManagersWrapper(ContactManager contactManager, PeerManager peerManager) {
-}
